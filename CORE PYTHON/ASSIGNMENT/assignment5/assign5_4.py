5d14ff428eb6e652434c3842e3cd01811427289e
omrthakur18-code <om.r.thakur.18@gmail.com>
2026-07-30 08:25:37 +0530

    Update assign5_4.py

--- CORE PYTHON/ASSIGNMENT/assignment5/assign5_4.py
+++ CORE PYTHON/ASSIGNMENT/assignment5/assign5_4.py
@@ -1,22 +1,21 @@
 # 4. WAP to print Armstrong number within a given range
 
-s=int(input('enter the starting range: '))
-e=int(input('enter the ending range: '))
-count=int(str(i))
 
-for i in range(s,e+1):
-    num=i
-    count=int(str(i))
-    
+s = int(input("Enter the starting range: "))
+e = int(input("Enter the ending range: "))
 
-    total=0
-    temp=num
-    while temp>0:
-        d=temp%10
-        total=total+(d**count)
-        temp=temp//10
+for i in range(s, e + 1):
 
-print(total)
-if(total==num):
+    num = i
+    count = len(str(i))      # Number of digits
 
-    print(i)
+    total = 0
+    temp = num
+
+    while temp > 0:
+        d = temp % 10
+        total = total + (d ** count)
+        temp = temp // 10
+
+    if total == num:
+        print(num)
