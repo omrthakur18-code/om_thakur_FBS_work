6eb7602c5e59f5ea7a9ce38bec33cf008187785b
omrthakur18-code <om.r.thakur.18@gmail.com>
2026-08-10 17:12:19 +0530

    test1_1.py

--- CORE PYTHON/TESTS/test1.py/test1_1.py
+++ CORE PYTHON/TESTS/test1.py/test1_1.py
@@ -1,3 +1,6 @@
+#calculate area and parameter of figure.
+
+
 l = int(input("Enter the length of figure: "))
 b = int(input("Enter the breadth of figure: "))
 r = int(input("Enter the radius of figure: "))
