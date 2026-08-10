7d92efa8c29b4783ad48c1afde3484736c77c3b6
omrthakur18-code <om.r.thakur.18@gmail.com>
2026-08-10 17:08:15 +0530

    test1_1.py

--- CORE PYTHON/TESTS/test1.py/test1_1.py
+++ CORE PYTHON/TESTS/test1.py/test1_1.py
@@ -4,7 +4,7 @@ r = int(input("Enter the radius of figure: "))
 
 area = (l*b) + (0.5 * 3.14 * r**2)
 
-parameter = 2*(l + b) + (3.14 * r + 2 * r)
+parameter = (2 * l) + b + (3.14 * r)
 
 print("Area of the given figure is:", area)
 print("Perimeter of the given figure is:", parameter)
