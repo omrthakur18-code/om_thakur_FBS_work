250a9461eaaf70674e833a1a0104a954e7b7af14
omrthakur18-code <om.r.thakur.18@gmail.com>
2026-08-26 12:18:54 +0530

    Add files via upload

---
+++ CORE PYTHON/DEMO/BASICS/pattern/pattern10.py
@@ -0,0 +1,10 @@
+for i in range(1,6):
+    for j in range(1,6):
+        if(i%2==1):
+            print('*',end=' ')
+        else:
+            print('$',end=' ')
+    print()
+
+
+    
---
+++ CORE PYTHON/DEMO/BASICS/pattern/pattern24.py
@@ -0,0 +1,14 @@
+for i in range(1,6):
+    for j in range(1,i+1):
+        print('*',end=' ')
+    print()
+
+# for i in range(1,5):
+#     for j in range(5-i,0,-1):
+#         print('*',end=' ')
+#     print()
+
+for i in range(1,5):
+    for j in range(1,6-i):
+        print('*',end=' ')
+    print()
